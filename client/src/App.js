b95cb646e071d80e4fae5563ecd5f05c1cfd38ad
Eric Devine <80430372+BigPorg@users.noreply.github.com>
2021-08-25 10:51:47 -0400

    src files

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,8 @@ import Nav from './components/Nav';
 import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
+import { Provider } from 'react-redux';
+import shop from './utils/shop'
 
 const httpLink = createHttpLink({
   uri: '/graphql',
