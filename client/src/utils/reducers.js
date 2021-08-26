f0a25c2e2ea3d595242cebf25354fe7dac6f4adb
Eric Devine <80430372+BigPorg@users.noreply.github.com>
2021-08-26 11:51:41 -0400

    tutor edits

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -20,7 +20,7 @@ const initialState = {
 };
 
 // The reducer is a function that accepts the current state and an action. It returns a new state based on that action.
-export const reducer = (state = inititalState, action) => {
+export const reducer = (state = initialState, action) => {
   switch (action.type) {
     // Returns a copy of state with an update products array. We use the action.products property and spread it's contents into the new array.
     case UPDATE_PRODUCTS:
@@ -46,7 +46,7 @@ export const reducer = (state = inititalState, action) => {
       return {
         ...state,
         cartOpen: true,
-        cart: state.cart.map((product) => {
+        cart: state.cart.map(product => {
           if (action._id === product._id) {
             product.purchaseQuantity = action.purchaseQuantity;
           }
@@ -57,7 +57,7 @@ export const reducer = (state = inititalState, action) => {
     // First we iterate through each item in the cart and check to see if the `product._id` matches the `action._id`
     // If so, we remove it from our cart and set the updated state to a variable called `newState`
     case REMOVE_FROM_CART:
-      let newState = state.cart.filter((product) => {
+      let newState = state.cart.filter(product => {
         return product._id !== action._id;
       });
 
