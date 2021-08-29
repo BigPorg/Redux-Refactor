1b2f9fa3a090f68a14858cf83af21a9fb9797d3f
Eric Devine <80430372+BigPorg@users.noreply.github.com>
2021-08-29 12:09:36 -0400

    testing comment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+// testing comment
+console.log("testing comment")
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/ReduxRefactor', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
